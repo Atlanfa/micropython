d18d5202a814013ad6339d543223ea45bb632a27
Ilia Bekishev <Atlanfa@users.noreply.github.com>
2020-05-29 10:02:16 +0300

    moved pins to other file

--- lab1/main.py
+++ lab1/main.py
@@ -1,10 +1,9 @@
 from lab1 import blink_word
-from machine import Pin
+import pins
 
-pin4 = Pin(4, Pin.IN, Pin.PULL_UP)      #настройка 4го пина, к которому подключена кнопка
 
 while True:
-    if not pin4.value():  # проверяем сигнал передающийся на пин, если 0 то разрешаем работу если 1 запрещаем
+    if not pins.pin4.value():  # проверяем сигнал передающийся на пин, если 0 то разрешаем работу если 1 запрещаем
         blink_word('SOS')
 
 
